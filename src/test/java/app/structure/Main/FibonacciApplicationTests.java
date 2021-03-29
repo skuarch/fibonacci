f648dcc76c669ec2e2b13e2adfa561b50bfae843
skuarch <skuarch@yahoo.com.mx>
2021-03-29 16:20:22 -0600

    New project

---
+++ src/test/java/app/structure/Main/FibonacciApplicationTests.java
@@ -0,0 +1,13 @@
+package app.structure.Main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FibonacciApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
